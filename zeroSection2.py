0981877de227918d12511dfd4e1a360633b2e12e
Yu-Jye(Jerry) Tung <yujyet@gmail.com>
2017-04-17 00:28:22 -0700

    update zeroSection2.py

---
+++ zeroSection2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import struct
+
+def main():
+	print "---------- zeroSection2 ----------"
+
+	#open the file as stream of binary in read and write 
+	filePath = sys.argv[1]
+	theFile = open(filePath, "r+b")
+	contents = theFile.read()
+
+	#figure out size of section headers table 
+	start =  struct.unpack("i", contents[32:36])[0]
+	entrySize = struct.unpack("i", contents[46:48]+b'\x00\x00')[0]
+	entryNum = struct.unpack("i", contents[48:50]+b'\x00\x00')[0]
+	totalSize = entrySize * entryNum
+
+	#zero out the whole section headers table 
+	theFile.seek(start)
+	theFile.write(b'\x00' * totalSize)
+	
+	#zero out section headers info. from ELF Header 
+	offsets = [0x20, 0x2e, 0x30]
+	bytes = 2
+	for i in range(len(offsets)): 
+		if i == 0:
+			bytes = 4
+		theFile.seek(offsets[i])	
+		theFile.write(b'\x00' * bytes)
+			
+	
+
+main()
